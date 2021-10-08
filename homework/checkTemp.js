4d5eb4d3a1cecce5d88b809cec5cc83de5adf39d
triinparts <92164068+triinparts@users.noreply.github.com>
2021-10-08 16:38:53 +0300

    Create checkTemp.js

---
+++ homework/checkTemp.js
@@ -0,0 +1,8 @@
+function checkTemp(temperatuur) {
+if (temperatuur >= 20)
+    return -1
+else if (temperatuur >=21 && temperatuur <=40)
+   return 0
+else if (temperatuur >=41 && temperatuur <=60)
+return 1
+}
